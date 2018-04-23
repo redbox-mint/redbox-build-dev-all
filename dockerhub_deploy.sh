96f53ce33cbfc34d636991dcdc2a388fd5a285c4
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2018-04-23 11:31:22 +0930

    Fixed dockerhub build to use travis_commit env variable

--- dockerhub_deploy.sh
+++ dockerhub_deploy.sh
@@ -6,7 +6,7 @@ docker login -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=qcifengineering/redbox
 docker pull $REPO
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH; fi`
-docker build -f Dockerfile -t $REPO:$COMMIT .
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker build -f Dockerfile -t $REPO:$TRAVIS_COMMIT .
+docker tag $REPO:$TRAVIS_COMMIT $REPO:$TAG
+docker tag $REPO:$TRAVIS_COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
 docker push $REPO
