22daae8a94602480ae18b612da475f7d767cbf89
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2018-04-23 10:39:11 +0930

    Changed Docker build to use locally built tar.gz

---
+++ dockerhub_deploy.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+cd support/docker/
+find ../../target -name 'redbox-distro-*-build.tar.gz' | xargs -I % cp % ./redbox.tar.gz
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+export REPO=qcifengineering/redbox
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH; fi`
+docker build -f Dockerfile -t $REPO:$COMMIT .
+docker tag $REPO:$COMMIT $REPO:$TAG
+docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker push $REPO
