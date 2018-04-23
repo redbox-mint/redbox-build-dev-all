955aa98ad2136118e4f034af790cb9cab2ab430c
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2018-04-23 11:20:14 +0930

    Added docker pull to deploy script

--- dockerhub_deploy.sh
+++ dockerhub_deploy.sh
@@ -4,6 +4,7 @@ docker cp $(docker ps -l -q):/build/src/target ./
 find ./target -name 'redbox-distro-*-build.tar.gz' | xargs -I % cp % ./redbox.tar.gz
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=qcifengineering/redbox
+docker pull $REPO
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH; fi`
 docker build -f Dockerfile -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
